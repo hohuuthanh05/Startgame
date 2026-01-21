c92444b590472be5b635b3fa09d652231ae9abd7
hohuuthanh05 <hohuuthanh123123@gmail.com>
2026-01-21 12:05:08 +0700

    Initial commit

---
+++ Assets/script/playercontroller.cs
@@ -0,0 +1,69 @@
+using System;
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float jumpForce = 15f;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private Transform groundCheck;
+    private Animator animator;
+
+    private bool isGrounded;
+
+
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        HandleMovement();
+        HandleJump();
+        UpdateAnimation();
+
+        
+    
+    }
+
+    private void HandleJump()
+{
+    if (Input.GetButtonDown("Jump")&& isGrounded) 
+    {
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+    }
+ isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+
+
+}
+
+    private void HandleMovement()
+    {
+        float moveInput = Input.GetAxis("Horizontal");
+        rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+
+        if (moveInput > 0)
+            transform.localScale = new Vector3(1, 1, 1);
+        else if (moveInput < 0)
+            transform.localScale = new Vector3(-1, 1, 1);
+    }
+    private void UpdateAnimation()
+    {
+        bool isRunning = Mathf.Abs(rb.linearVelocity.x) > 0.1f;
+        bool isJumping = !isGrounded;
+        animator.SetBool("isRunning", isRunning);
+        animator.SetBool("isJumping", isJumping);
+ 
+        
+    }
+}
