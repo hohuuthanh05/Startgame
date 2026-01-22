ceb635d7e7f5f45915a6230c5d904ae20eb039f2
hohuuthanh05 <hohuuthanh123123@gmail.com>
2026-01-22 15:54:05 +0700

    update
    gun

--- Assets/script/WeaponManager.cs
+++ Assets/script/WeaponManager.cs
@@ -1,50 +1,16 @@
 using UnityEngine;
 
-public class WeaponManager : MonoBehaviour
+public class weaponManager : MonoBehaviour
 {
-    [Header("Weapon GameObjects")]
-    [SerializeField] private GameObject gun;
-    [SerializeField] private GameObject sword;
-
-    [Header("Weapon Scripts (optional)")]
-    [SerializeField] private MonoBehaviour gunScript;   // Gun.cs
-    [SerializeField] private MonoBehaviour swordScript; // Sword.cs (nếu có)
-
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        EquipGun(); // mặc định cầm súng khi vào game
+        
     }
 
+    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            EquipGun();
-        }
-
-        if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            EquipSword();
-        }
-    }
-
-    void EquipGun()
-    {
-        // bật / tắt GameObject
-        gun.SetActive(true);
-        sword.SetActive(false);
-
-        // bật / tắt script (RẤT QUAN TRỌNG)
-        if (gunScript != null) gunScript.enabled = true;
-        if (swordScript != null) swordScript.enabled = false;
-    }
-
-    void EquipSword()
-    {
-        gun.SetActive(false);
-        sword.SetActive(true);
-
-        if (gunScript != null) gunScript.enabled = false;
-        if (swordScript != null) swordScript.enabled = true;
+        
     }
 }
