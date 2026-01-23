9499a043d4286fe9dbc67c5aa319f63ab32e0e05
Ahy18 <huycv18.work@gmail.com>
2026-01-23 16:04:40 +0700

    feat: Update PlayerController for improved jump mechanics and adjust move speed fix: Modify ground layer settings and layout adjustments in scene files

--- Assets/script/playercontroller.cs
+++ Assets/script/playercontroller.cs
@@ -5,14 +5,15 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float jumpForce = 15f;
+    [SerializeField] private int maxJumpCount = 2;
+    [SerializeField] private float fallMultiplier = 2.5f;
     [SerializeField] private LayerMask groundLayer;
     [SerializeField] private Transform groundCheck;
     private Animator animator;
-
-    private bool isGrounded;
-
-
     private Rigidbody2D rb;
+    private bool isGrounded;
+    private int jumpCount;
+    private float moveInput;
 
     private void Awake()
     {
@@ -26,24 +27,31 @@ public class PlayerController : MonoBehaviour
     }
 
     void Update()
-    {
-        HandleMovement();
-        HandleJump();
+    {   
+        moveInput = Input.GetAxis("Horizontal");
+        CheckGround();
+        HandleJumpInput();
         UpdateAnimation();
     }
 
-    private void HandleJump()
-{
-    isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
-    if (Input.GetButtonDown("Jump") && isGrounded) 
+    private void FixedUpdate()
     {
-        rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        HandleMovement();
+        BetterJumpPhysics();
     }
-}
+
+    private void CheckGround()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+
+        if (isGrounded)
+        {
+            jumpCount = 0;
+        }
+    }
 
     private void HandleMovement()
     {
-        float moveInput = Input.GetAxis("Horizontal");
         rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
 
         if (moveInput > 0)
@@ -51,12 +59,28 @@ public class PlayerController : MonoBehaviour
         else if (moveInput < 0)
             transform.localScale = new Vector3(-1, 1, 1);
     }
+
+    private void HandleJumpInput()
+    {
+        if (Input.GetButtonDown("Jump") && jumpCount < maxJumpCount)
+        {   
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0);
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            jumpCount++;
+        }
+    }
+
+    private void BetterJumpPhysics()
+    {
+        if (rb.linearVelocity.y < 0)
+        {
+            rb.linearVelocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.fixedDeltaTime;
+        }
+    }
+
     private void UpdateAnimation()
     {
-        bool isRunning = Mathf.Abs(rb.linearVelocity.x) > 0.1f;
-        bool isJumping = !isGrounded && rb.linearVelocity.y > 0.05f;
-        animator.SetBool("isRunning", isRunning);
-        animator.SetBool("isJumping", isJumping);
-
+        animator.SetBool("isRunning", Mathf.Abs(rb.linearVelocity.x) > 0.1f);
+        animator.SetBool("isJumping", !isGrounded);
     }
 }
